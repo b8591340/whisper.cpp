77f78846bfa8fe17f2d36731bdc6783f03575fa1
b8591340 <70052269+b8591340@users.noreply.github.com>
2023-11-16 19:42:32 +0100

    Use CoreML

--- Package.swift
+++ Package.swift
@@ -63,10 +63,7 @@ let package = Package(
             publicHeadersPath: "spm-headers",
             cSettings: [
                 .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
-                .define("GGML_USE_ACCELERATE")
-                // NOTE: NEW_LAPACK will required iOS version 16.4+
-                // We should consider add this in the future when we drop support for iOS 14
-                // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
+                .define("GGML_USE_ACCELERATE"),
                 .define("ACCELERATE_NEW_LAPACK"),
                 .define("ACCELERATE_LAPACK_ILP64")
             ] + additionalSettings,
